9293edfd70acc16b746538c33d503b305b5457e1
Tim Blazic <timblazic.info@gmail.com>
2026-03-19 10:42:16 +0100

    add env

--- src/components/TimberWidget.tsx
+++ src/components/TimberWidget.tsx
@@ -6,10 +6,10 @@ export function TimberWidget() {
   useEffect(() => {
     import("timber-feedback-sdk").then(({ init }) => {
       init({
-        projectId: "07d01d0e-8948-4dcd-b62c-d47e0a733edc",
-        apiKey: "5e937cad8e1c39537bffd205d336e13410165b5be89d605306f53a3dc73789d8",
-        apiUrl: "https://www.timber.report/api/v1",
-        screenshotApiUrl: "/api/timber/screenshot",
+        projectId: process.env.NEXT_PUBLIC_TIMBER_PROJECT_ID!,
+        apiKey: process.env.NEXT_PUBLIC_TIMBER_API_KEY!,
+        apiUrl: process.env.NEXT_PUBLIC_TIMBER_API_URL!,
+        screenshotApiUrl: process.env.NEXT_PUBLIC_TIMBER_SCREENSHOT_API_URL!,
         position: "bottom-right",
         theme: "auto",
       });
