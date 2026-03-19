f037a515b37d84a9e7ce536ca27e780e9b708c26
Tim Blazic <timblazic.info@gmail.com>
2026-03-19 13:26:16 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["playwright-core", "@sparticuz/chromium-min"],
   transpilePackages: ["timber-feedback-sdk"],
 };
 
